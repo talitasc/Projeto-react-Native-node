106872b7da898b5a6e84f2fc81ef2f2565726798
Talita Castro <talita.santos.castro@gmail.com>
2020-03-31 00:55:42 -0300

    mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,21 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+   <Routes/> 
+  );
+}
+
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
